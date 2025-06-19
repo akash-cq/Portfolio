c0ec98f784dbc8a30c8007d70b272234e635e41e
Akash gupta <ag244834@gmail.com>
2025-06-19 10:14:07 +0530

    fix issues

--- src/Components/sub-folder/Education/Education.jsx
+++ src/Components/sub-folder/Education/Education.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Cards from '../cards'
-import { education } from '../../../constant'
+import React from "react";
+import Cards from "../cards";
+import { education } from "../../../constant";
 export default function Education() {
   return (
-    <Cards contents={education} cardFor='EDUCATION' desc='my education background '>
-      
-    </Cards>
-  )
+    <section id="education">
+      <Cards
+        contents={education}
+        cardFor="EDUCATION"
+        desc="my education background "
+      ></Cards>
+    </section>
+  );
 }
--- src/Components/sub-folder/Experince/Experince.jsx
+++ src/Components/sub-folder/Experince/Experince.jsx
@@ -3,10 +3,12 @@ import { experiences } from "../../../constant";
 import Cards from "../cards";
 export default function Experince() {
   return (
-    <Cards
-      contents={experiences}
-      cardFor="EXPERIENCE"
-      desc="A collection of my work experience and the roles i have taken various organisation."
-    />
+    <section id="experience">
+      <Cards
+        contents={experiences}
+        cardFor="EXPERIENCE"
+        desc="A collection of my work experience and the roles i have taken various organisation."
+      />
+    </section>
   );
 }
